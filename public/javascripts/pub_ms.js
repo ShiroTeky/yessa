5281d115d177f34b7c8d9853d7aeb627dc003db2
Hubert N'Guessan <Hubert N'Guessan>
2016-11-14 22:13:46 +0000

    Updated design

---
+++ public/javascripts/pub_ms.js
@@ -0,0 +1,11 @@
+var ccs_cc_args = ccs_cc_args || [];
+            (function() {
+              var o = ccs_cc_args; o.push(['_SKey', '730df480']); o.push(['_ZoneId', 'db1ee9db45']);
+              var o1 = ccs_cc_args; o1.push(['_SKey', '730df480']); o1.push(['_ZoneId', 'db1ee9db46']);
+              var sc = document.createElement('script'); 
+              sc.type = 'text/javascript'; 
+              sc.async = true;
+              sc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.cnetcontent.com/jsc/h.js';
+              var n = document.getElementsByTagName('script')[0];
+              n.parentNode.insertBefore(sc, n);
+            })();
---
+++ public/javascripts/pub_mso365.js
@@ -0,0 +1,7 @@
+var ccs_cc_args = ccs_cc_args || [];
+(function () {
+   var o = ccs_cc_args; o.push(['_SKey', '730df480']); o.push(['_ZoneId', 'emdedFRresellerthenewofficeoffice2016office365']); 
+   var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
+   sc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.cnetcontent.com/jsc/h.js';
+   var n = document.getElementsByTagName('script')[0]; n.parentNode.insertBefore(sc, n);
+})();
